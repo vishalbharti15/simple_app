67e85a8a90adfafa569bef885a434d7e2de9b03c
vishal15 <vbhaarti@gmail.com>
2022-02-01 12:37:50 +0530

    1st change

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express")
 var app = express()
-var port = 3010
+var port = process.env.PORT || 5000
 
 app.get("/", function(req, res){
-    res.send("<body style='background-color:grey;color:white'><h1 style='margintop:300px'><center>Welcome Page</center></h1></body>")
+    res.send("<h1><center>Welcome Page</center></h1>")
 })
 
 app.get("/about", function(req, res){
-    res.send("<body style='background-color:grey;color:white'><h1 style='margintop:300px'><center>About us Page</center></h1></body>")
-})
-
-app.get("/blog", function(req, res){
-    res.send("<body style='background-color:grey;color:white'><h1 style='margintop:300px'><center>Blog Page</center></h1></body>")
+    res.send("<h1><center>About Page</center></h1>")
 })
 
 app.listen(port, function(err){

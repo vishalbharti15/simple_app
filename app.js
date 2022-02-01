0d69a0c72d7e9ed17a2050849682cb884b6d4fcb
vishal15 <vbhaarti@gmail.com>
2022-02-01 19:13:09 +0530

    Added Contact Us Page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/about", function(req, res){
     res.send("<h1><center>About Page</center></h1>")
 })
 
+app.get("/contact", function(req, res){
+    res.send("<h1><center>Contact Us Page</center></h1>")
+})
+
 app.listen(port, function(err){
     console.log("Server started at port ", port)
 })
